58245ac100fcb39a7eb09396d2fef75868f966a0
Ram183 <ramcrazy1992@gmail.com>
2021-01-11 18:03:40 +0530

    Adding a new project

---
+++ src/org/democlass/github/Git_Hub_Class.java
@@ -0,0 +1,19 @@
+package org.democlass.github;
+
+public class Git_Hub_Class {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Java");
+		
+		System.out.println("Selenium");
+		
+		System.out.println("JavaSelenium");
+		
+		System.out.println("Vishal");
+	
+		
+	}
+
+}
